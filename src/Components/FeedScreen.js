8ffdbd48a331b9305aad0568f5ccc3783c856770
Annie <anniekao@posteo.net>
2019-09-25 20:50:36 -0400

    Modify map screen header, add useeffect to feed

--- src/Components/FeedScreen.js
+++ src/Components/FeedScreen.js
@@ -1,7 +1,25 @@
-import React from "react";
-import { Container, Content, Text, Button, Header, Left, Right, Body } from 'native-base';
+import React, { useEffect, useState } from "react";
+import { Container, Content, Text, Button, Header, Left, Right, Body, DeckSwiper } from 'native-base';
+import axios from 'axios';
 
 const FeedScreen = ({navigation}) => {
+  let [photos, setPhotos] = useState([]);
+
+   useEffect(() => {
+    axios
+      .all([
+        axios.get('https://source.unsplash.com/random/400x350'),
+        axios.get('https://source.unsplash.com/random/400x350'),
+        axios.get('https://source.unsplash.com/random/400x350'),
+        axios.get('https://source.unsplash.com/random/400x350'),
+        axios.get('https://source.unsplash.com/random/400x350')
+      ])
+      .then(data => {
+        console.log(data[0]);
+      })
+      .catch(err => console.error(err));
+   }, [])
+
    return (
     <Container>
       <Header style={{backgroundColor:'#f46036'}}>
--- src/Components/MapScreen.js
+++ src/Components/MapScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Content, Text, Button} from 'native-base';
+import { Container, Content, Text, Button, Header, Left, Right, Body} from 'native-base';
 
 const MapScreen = ({navigation}) => {
   navigationOptions = {
@@ -8,6 +8,13 @@ const MapScreen = ({navigation}) => {
 
    return (
     <Container>
+       <Header style={{backgroundColor:'#f46036'}}>
+        <Left style={{flex:1}}/>
+        <Body style={{flex:1, alignItems:'center', justifyContent: "center"}}>
+          <Text style={{color:'#fdfffc', fontWeight:'bold', fontSize:20}}>Map</Text>
+        </Body>
+        <Right style={{flex:1}}/>
+      </Header>
       <Content
         contentContainerStyle={{ flex: 1, alignItems: "center", justifyContent: "center" }}
       >
